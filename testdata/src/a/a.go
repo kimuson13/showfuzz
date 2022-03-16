9f27d0b19f3c237bce8b5155a570dc3467eb6233
kimuson <kimuson@jxpress.net>
2022-03-16 16:25:26 +0900

    feat: create fuzzable check analyzer

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,17 +1,19 @@
 package a
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
-}
+type MyInt int
 
-func canFuzz1(int) error {} // want "can fuzz test"
+func canFuzz1(int) error { // want "can fuzz test"
+	return nil
+}
 
 func canFuzz2(string, []byte) {} // want "can fuzz test"
 
 func canFuzz3(float32, float64, bool) {} // want "can fuzz test"
 
+func canFuzz4(MyInt) {} // want "can fuzz test"
+
 func cantFuzz1(error) {}
 
 func cantFuzz2(any) {}
+
+func cantFuzz3() {}
