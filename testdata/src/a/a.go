8b3a678e029b8dd0bd0b60da9cae0ab9d7178326
kimuson <kimuson@jxpress.net>
2022-03-16 14:57:29 +0900

    test: add expected testcase

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -5,3 +5,13 @@ func f() {
 	var gopher int // want "pattern"
 	print(gopher)  // want "identifier is gopher"
 }
+
+func canFuzz1(int) error {} // want "can fuzz test"
+
+func canFuzz2(string, []byte) {} // want "can fuzz test"
+
+func canFuzz3(float32, float64, bool) {} // want "can fuzz test"
+
+func cantFuzz1(error) {}
+
+func cantFuzz2(any) {}
